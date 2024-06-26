2f23c13bba2745594bfb01589b62c26a3be761cb
Jhorman Tito <jhormanrus@gmail.com>
2024-06-25 22:28:20 -0500

    🩹 update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{vue,ts}',
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
   ],
   theme: {
     extend: {},
