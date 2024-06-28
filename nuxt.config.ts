ce940f1b8b9666b7f131c1cce748da027189ca32
Jhorman Tito <jhormanrus@gmail.com>
2024-06-27 23:16:24 -0500

    🚀 use tailwind plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  modules: ["@nuxtjs/tailwindcss"],
 })
